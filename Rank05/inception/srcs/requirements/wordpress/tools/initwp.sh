f49a0efce94faf318879d8783cfb5b5c00ba8812
daxferna <daxferna@Debian.myguest.virtualbox.org>
2026-07-25 18:44:57 +0200

    Fixed bug

--- Rank05/inception/srcs/requirements/wordpress/tools/initwp.sh
+++ Rank05/inception/srcs/requirements/wordpress/tools/initwp.sh
@@ -2,6 +2,16 @@
 
 echo "----------- Wordpress: Initializing... ----------"
 
+# Read environment variables (they come from .env via docker-compose)
+export SQL_DATABASE="${SQL_DATABASE}"
+export SQL_USER="${SQL_USER}"
+export WP_URL="${WP_URL}"
+export WP_TITLE="${WP_TITLE}"
+export WP_ADMIN="${WP_ADMIN}"
+export WP_ADMIN_EMAIL="${WP_ADMIN_EMAIL}"
+export WP_USER="${WP_USER}"
+export WP_USER_EMAIL="${WP_USER_EMAIL}"
+
 WP_PATH="/var/www/html"
 
 # Get credentials from secrets (automattically mounted in /run)
@@ -12,7 +22,6 @@ WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
 # Ensure PHP runtime dir exists
 mkdir -p /run/php
-
 cd /var/www/wordpress
 
 # Wait until mariadb is up
@@ -21,35 +30,33 @@ while ! printf "" 2>>/dev/null >/dev/tcp/mariadb/3306; do
 	sleep 1
 done
 
-## If config not set, run
 if [ ! -f wp-config.php ]; then
-
-	# creates wp-config.php (link wp to db)
+# creates wp-config.php (link wp to db)
 	wp config create \
-	--allow-root \				# Allows root access
-	--dbname=$SQL_DATABASE \
-	--dbuser=$SQL_USER \
-	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 \		# DB host (MariaDB)
-	--path='/var/www/wordpress'	# Path to wordpress
-
-	# finish initial WP instalation (adds admin)
-	core install \
 	--allow-root \
-	--url=$WP_URL \
-	--title=$WP_TITLE \
-	--admin_user=$WP_ADMIN \
-	--admin_password=$WP_ADMIN_PASSWORD \
-	--admin_email=$WP_ADMIN_EMAIL \
+	--dbname="$SQL_DATABASE" \
+	--dbuser="$SQL_USER" \
+	--dbpass="$SQL_PASSWORD" \
+	--dbhost=mariadb:3306 \
 	--path='/var/www/wordpress'
 
-	# creates normal user
-	user create $WP_USER $WP_USER_EMAIL \
-	--user_pass=$WP_USER_PASSWORD \
+# finish initial WP instalation (adds admin)
+	wp core install \
+	--allow-root \
+	--url="$WP_URL" \
+	--title="$WP_TITLE" \
+	--admin_user="$WP_ADMIN" \
+	--admin_password="$WP_ADMIN_PASSWORD" \
+	--admin_email="$WP_ADMIN_EMAIL" \
+	--path='/var/www/wordpress'
+
+# creates normal user
+	wp user create "$WP_USER" "$WP_USER_EMAIL" \
+	--user_pass="$WP_USER_PASSWORD" \
 	--role=author \
 	--allow-root
-	# changes WP files ownership to www-data (the one that runs PHP-FPM)
-	# www-data has now permission to do certain things, without having full root permissions
+# changes WP files ownership to www-data (the one that runs PHP-FPM)
+# www-data has now permission to do certain things, without having full root permissions
 	chown -R www-data:www-data /var/www/wordpress
 fi
 
