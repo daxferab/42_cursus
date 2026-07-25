0d4b42d69e36691aff9831f6c1ea239632be086d
daxferna <daxferna@proton.me>
2026-07-25 22:00:57 +0200

    Added new lines

--- Rank05/inception/srcs/requirements/wordpress/tools/initwp.sh
+++ Rank05/inception/srcs/requirements/wordpress/tools/initwp.sh
@@ -12,7 +12,7 @@ export WP_ADMIN_EMAIL="${WP_ADMIN_EMAIL}"
 export WP_USER="${WP_USER}"
 export WP_USER_EMAIL="${WP_USER_EMAIL}"
 
-WP_PATH="/var/www/html"
+WP_PATH="/var/www/wordpress"
 
 # Get credentials from secrets (automattically mounted in /run)
 SQL_PASSWORD=$(cat /run/secrets/db_user_password)
@@ -22,7 +22,7 @@ WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
 # Ensure PHP runtime dir exists
 mkdir -p /run/php
-cd /var/www/wordpress
+cd $WP_PATH
 
 # Wait until mariadb is up
 while ! printf "" 2>>/dev/null >/dev/tcp/mariadb/3306; do
@@ -38,7 +38,7 @@ if [ ! -f wp-config.php ]; then
 	--dbuser="$SQL_USER" \
 	--dbpass="$SQL_PASSWORD" \
 	--dbhost=mariadb:3306 \
-	--path='/var/www/wordpress'
+	--path='$WP_PATH'
 
 # finish initial WP instalation (adds admin)
 	wp core install \
@@ -48,7 +48,7 @@ if [ ! -f wp-config.php ]; then
 	--admin_user="$WP_ADMIN" \
 	--admin_password="$WP_ADMIN_PASSWORD" \
 	--admin_email="$WP_ADMIN_EMAIL" \
-	--path='/var/www/wordpress'
+	--path='$WP_PATH'
 
 # creates normal user
 	wp user create "$WP_USER" "$WP_USER_EMAIL" \
@@ -57,7 +57,7 @@ if [ ! -f wp-config.php ]; then
 	--allow-root
 # changes WP files ownership to www-data (the one that runs PHP-FPM)
 # www-data has now permission to do certain things, without having full root permissions
-	chown -R www-data:www-data /var/www/wordpress
+	chown -R www-data:www-data $WP_PATH
 fi
 
 # Start PHP-FPM as PID 1
